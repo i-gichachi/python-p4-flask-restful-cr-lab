6f773ba32e2f9c0f792be77ee7e27c95649a6ecc
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-10-19 16:52:14 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = True
+app.json_encoder = None
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,11 +18,32 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return jsonify([plant.serialize() for plant in plants])
 
+    def post(self):
+        data = request.get_json()
+        name = data.get('name')
+        image = data.get('image', None)  # Allow image to be nullable
+        price = data.get('price')
+        if not name or price is None:
+            return make_response(jsonify({"error": "Missing data"}), 400)
+
+        plant = Plant(name=name, image=image, price=price)
+        db.session.add(plant)
+        db.session.commit()
+        return jsonify(plant.serialize()), 201
+    
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+        if plant is None:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        return jsonify(plant.serialize())
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -6,4 +6,27 @@ db = SQLAlchemy()
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
+    serialize_rules = ('-id',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=True)  
+    price = db.Column(db.Float, nullable=False)
+
+    serialize_rules = ('-id',)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "price": self.price
+        }
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "price": self.price
+        }
